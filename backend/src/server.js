8b7d9ca746084e9e82b05634503fedbd0809e593
hoangphuc210105 <tranhoangphuc210105@gmail.com>
2026-05-08 17:11:42 +0700

    Chuẩn bị deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,18 +3,30 @@ import testRoutes from "./routes/testRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import path, { join } from "path";
 
 dotenv.config();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 const app = express();
 
 //Middlewares
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173" })); // Thay đổi URL nếu frontend chạy trên cổng khác
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors({ origin: "http://localhost:5173" })); // Thay đổi URL nếu frontend chạy trên cổng khác
+}
 
 app.use(express.json());
 app.use("/api/test", testRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+    })
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
